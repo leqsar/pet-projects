26b08bc03a53819fb5cc554a7a5117c790671eaf
Aleksandra Ivanova <arteg132579@gmail.com>
2024-04-17 17:03:35 +0300

    style: add links hover effects; add styles for the current active link

--- src/app/ui/spaceTourism/nav-links.tsx
+++ src/app/ui/spaceTourism/nav-links.tsx
@@ -2,16 +2,17 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styles from '@/app/ui/spaceTourism/navigation.module.css'
+import clsx from 'clsx';
 
 const links = [
-  { name: 'Home', href: '/spaceTourism', number: "00" },
-  { name: 'Destination', href: '/spaceTourism/destination', number: "01" },
-  { name: 'Crew', href: '/spaceTourism/crew', number: "02" },
-  { name: 'Technology', href: '/spaceTourism/technology', number: "03" },
+  { name: 'Home', href: '/projects/spaceTourism', number: "00" },
+  { name: 'Destination', href: '/projects/spaceTourism/destination', number: "01" },
+  { name: 'Crew', href: '/projects/spaceTourism/crew', number: "02" },
+  { name: 'Technology', href: '/projects/spaceTourism/technology', number: "03" },
 ];
 
 export default function NavLinks() {
-  // const pathname = usePathname();
+  const pathname = usePathname();
 
   return (
     <div className={styles.linksWrapper}>
@@ -20,7 +21,9 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={styles.linkWrapper}
+            className={clsx( styles.linkWrapper, {
+                [styles.activeLink]: pathname === link.href,
+            })}
           >
             <p className={styles.name}>{link.name}</p>
             <p className={styles.number}>{link.number}</p>
@@ -30,6 +33,8 @@ export default function NavLinks() {
     </div>
   );
 }
+
+// styles.linkWrapper
 
 // className={clsx(
 //   'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
